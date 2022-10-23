80c0c1c03084f7906625bff31ccfba9e43fa06db
S.M.R.T <99488633+ThePhenomenalP@users.noreply.github.com>
2022-10-24 02:21:42 +0330

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseApiUrl: 'https://localhost:7279',
+  production: false
 };
 
 /*
